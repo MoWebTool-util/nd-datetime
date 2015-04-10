c1ee9febddd5760e8a887afb370a4088e1ef0aab
crossjs <liwenfu@crossjs.com>
2015-04-10 18:09:04 +0800

    update

--- index.js
+++ index.js
@@ -5,80 +5,191 @@
 
 'use strict';
 
+var MONTH_NAMES = ['January', 'February', 'March',  'April', 'May', 'June',  'July', 'August', 'September',  'October', 'November', 'December'];
+var MONTH_NAMES_ABBR = ['Jan', 'Feb', 'Mar',  'Apr', 'May', 'Jun',  'Jul', 'Aug', 'Sep',  'Oct', 'Nov', 'Dec'];
+var DAY_NAMES = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var DAY_NAMES_ABBR = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+var DATETIME_FORMAT = 'yyyy-MM-dd hh:mm:ss';
+
+function zeroPad(m, n) {
+  n || (n = 2);
+  m = '' + m;
+  n -= m.length;
+  while (n--) {
+    m = '0' + m;
+  }
+  return m;
+}
+
+var helpers = {
+
+  MMMM: function(date) {
+    return MONTH_NAMES[helpers.M(date) - 1];
+  },
+
+  MMM: function(date) {
+    return MONTH_NAMES_ABBR[helpers.M(date) - 1];
+  },
+
+  EEEE: function(date) {
+    return DAY_NAMES[helpers.D(date)];
+  },
+
+  EEE: function(date) {
+    return DAY_NAMES_ABBR[helpers.D(date)];
+  },
+
+  // TODO: 一年中的第几周
+  EE: function(/*date*/) {
+    return '';
+  },
+
+  // TODO: 一月中的第几周
+  E: function(/*date*/) {
+    return '';
+  },
+
+  D: function(date) {
+    return date.getDay();
+  },
+
+  yyyy: function(date) {
+    return date.getFullYear();
+  },
+
+  yy: function(date) {
+    return date.getYear();
+  },
+
+  MM: function(date) {
+    return zeroPad(helpers.M(date));
+  },
+
+  M: function(date) {
+    return date.getMonth() + 1;
+  },
+
+  dd: function(date) {
+    return zeroPad(helpers.d(date));
+  },
+
+  d: function(date) {
+    return date.getDate();
+  },
+
+  hh: function(date) {
+    return zeroPad(helpers.h(date));
+  },
+
+  h: function(date) {
+    return date.getHours();
+  },
+
+  mm: function(date) {
+    return zeroPad(helpers.m(date));
+  },
+
+  m: function(date) {
+    return date.getMinutes();
+  },
+
+  ss: function(date) {
+    return zeroPad(helpers.s(date));
+  },
+
+  s: function(date) {
+    return date.getSeconds();
+  },
+
+  ii: function(date) {
+    return zeroPad(helpers.i(date), 3);
+  },
+
+  i: function(date) {
+    return date.getMilliseconds();
+  }
+
+};
+
+function parseVal(timestamp, format) {
+  format || (format = DATETIME_FORMAT);
+  // '2015-04-10 14:43:05'
+  // 'yyyy-MM-dd hh:mm:ss'
+  // ["2015", "04", "10", "14", "43", "05"]
+  // ["yyyy", "MM", "dd", "hh", "mm", "ss"]
+
+  var tArr = timestamp.split(/\D+/);
+  var fArr = format.split(/[^yMdhmsiED]+/);
+
+  var y, M, d, h, m, s, i;
+  var map = {};
+
+  for (i = 0; i < fArr.length; i++) {
+    map[fArr[i]] = parseInt(tArr[i], 10);
+  }
+
+  y = map.yyyy || map.yy || 1970;
+  M = map.MM || map.M || 0;
+  d = map.dd || map.d || 1;
+  h = map.hh || map.h || 0;
+  m = map.mm || map.m || 0;
+  s = map.ss || map.s || 0;
+  i = map.ii || map.i || 0;
+
+  return new Date(y < 1900 ? y + 1900 : y, M - 1, d, h, m, s, i);
+}
+
+function parseDate(timestamp, format) {
+  if (typeof timestamp === 'function') {
+    return timestamp(format);
+  }
+
+  if (timestamp) {
+
+    if (timestamp.constructor === Date) {
+      return timestamp;
+    }
+
+    if (!/\D/.test(timestamp)) {
+      return new Date(+timestamp);
+    }
+
+    // string
+    return parseVal(timestamp, format);
+
+  } else {
+
+    if (typeof timestamp === 'undefined') {
+      // now
+      return new Date();
+    }
+
+    // 1970-01-01 ...
+    return new Date(0);
+
+  }
+}
+
 var DateTime = function(timestamp, format) {
-  this._date = timestamp ? new Date(timestamp) : new Date();
-  this._format = format || 'yy-mm-dd hh:ii:ss';
+  this._date = parseDate(timestamp, format);
+  this._format = format || 'yyyy-MM-dd hh:mm:ss';
 };
 
 DateTime.prototype.format = function(format) {
-  var that = this;
+  var date = this._date;
 
-  return (format || this._format).replace(/(y|m|d|h|i|s)\1?/g, function($0) {
-    var f = that['_' + $0];
-    return f ? f.call(that) : '';
+  return (format || this._format).replace(/(y|M|d|h|m|s|i|E|D)+/g, function($0) {
+    return ($0 in helpers) ? helpers[$0](date) : '';
   });
 };
 
-DateTime.prototype.toString = function() {
-  return this.format();
+DateTime.prototype.toNumber = function() {
+  return this._date.getTime();
 };
 
-DateTime.prototype._y = function() {
-  return this._date.getYear();
-};
-
-DateTime.prototype._yy = function() {
-  return this._date.getFullYear();
-};
-
-DateTime.prototype._m = function() {
-  return this._date.getMonth() + 1;
-};
-
-DateTime.prototype._mm = function() {
-  var m = this._m();
-
-  return m < 10 ? '0' + m : m;
-};
-
-DateTime.prototype._d = function() {
-  return this._date.getDate();
-};
-
-DateTime.prototype._dd = function() {
-  var d = this._d();
-
-  return d < 10 ? '0' + d : d;
-};
-
-DateTime.prototype._h = function() {
-  return this._date.getHours();
-};
-
-DateTime.prototype._hh = function() {
-  var h = this._h();
-
-  return h < 10 ? '0' + h : h;
-};
-
-DateTime.prototype._i = function() {
-  return this._date.getMinutes();
-};
-
-DateTime.prototype._ii = function() {
-  var i = this._i();
-
-  return i < 10 ? '0' + i : i;
-};
-
-DateTime.prototype._s = function() {
-  return this._date.getSeconds();
-};
-
-DateTime.prototype._ss = function() {
-  var s = this._s();
-
-  return s < 10 ? '0' + s : s;
+DateTime.prototype.toString = function(format) {
+  return this.format(format);
 };
 
 module.exports = function(timestamp, format) {
--- tests/index-spec.js
+++ tests/index-spec.js
@@ -1,14 +1,220 @@
 'use strict';
 
-var expect = require('expect.js');
-var Datetime = require('../index');
+var expect = require('expect.js')
+var datetime = require('../index')
 
 /*globals describe, it*/
 
 describe('nd-datetime', function() {
 
-  it('normal usage', function() {
+describe('1', function() {
 
-  });
+  var ts = 342122630006
+  var d = '1980-11-04'
+  var t = '02:03:50'
+  var dt = d + ' ' + t
+  var ms = '006'
+  var dtms = dt + '.' + ms;
 
-});
+  describe('timestamp', function() {
+
+    it('default', function() {
+      expect(datetime(ts).format()).to.be(dt)
+    })
+
+    it('week', function() {
+      expect(datetime(ts, 'EEEE').format()).to.be('Tuesday')
+      expect(datetime(ts, 'EEE').format()).to.be('Tue')
+      expect(datetime(ts, 'D').format()).to.be('2')
+    })
+
+    it('year', function() {
+      expect(datetime(ts, 'yyyy').format()).to.be('1980')
+      expect(datetime(ts, 'yy').format()).to.be('80')
+    })
+
+    it('month', function() {
+      expect(datetime(ts, 'MMMM').format()).to.be('November')
+      expect(datetime(ts, 'MMM').format()).to.be('Nov')
+      expect(datetime(ts, 'MM').format()).to.be('11')
+      expect(datetime(ts, 'M').format()).to.be('11')
+    })
+
+    it('date', function() {
+      expect(datetime(ts, 'dd').format()).to.be('04')
+      expect(datetime(ts, 'd').format()).to.be('4')
+    })
+
+    it('hours', function() {
+      expect(datetime(ts, 'hh').format()).to.be('02')
+      expect(datetime(ts, 'h').format()).to.be('2')
+    })
+
+    it('minutes', function() {
+      expect(datetime(ts, 'mm').format()).to.be('03')
+      expect(datetime(ts, 'm').format()).to.be('3')
+    })
+
+    it('seconds', function() {
+      expect(datetime(ts, 'ss').format()).to.be('50')
+      expect(datetime(ts, 's').format()).to.be('50')
+    })
+
+    it('milliseconds', function() {
+      expect(datetime(ts, 'ii').format()).to.be(ms)
+      expect(datetime(ts, 'i').format()).to.be('6')
+    })
+
+  })
+
+  describe('datetime', function() {
+
+    it('yyyy-MM-dd hh:mm:ss', function() {
+      expect(datetime(dt, 'yyyy-MM-dd hh:mm:ss').format()).to.be(dt)
+    })
+
+    it('yyyy-MM-dd', function() {
+      expect(datetime(dt, 'yyyy-MM-dd hh:mm:ss').format('yyyy-MM-dd')).to.be(d)
+      expect(datetime(dt, 'yyyy-MM-dd').format()).to.be(d)
+      expect(datetime(d, 'yyyy-MM-dd hh:mm:ss').format('yyyy-MM-dd')).to.be(d)
+      expect(datetime(d, 'yyyy-MM-dd').format()).to.be(d)
+    })
+
+    it('hh:mm:ss', function() {
+      expect(datetime(dt, 'yyyy-MM-dd hh:mm:ss').format('hh:mm:ss')).to.be(t)
+      expect(datetime(t, 'hh:mm:ss').format()).to.be(t)
+    })
+
+    it('ii', function() {
+      expect(datetime(ms, 'ii').format()).to.be(ms)
+    })
+
+    it('toNumber()', function() {
+      expect(datetime(dtms, 'yyyy-MM-dd hh:mm:ss.ii').toNumber()).to.be(ts)
+    })
+
+  })
+
+})
+
+describe('2', function() {
+
+  var ts = 1428648185116
+  var d = '2015-04-10'
+  var t = '14:43:05'
+  var dt = d + ' ' + t
+  var ms = '116'
+  var dtms = dt + '.' + ms;
+
+  describe('timestamp', function() {
+
+    it('default', function() {
+      expect(datetime(ts).format()).to.be(dt)
+    })
+
+    it('yyyy-MM-dd hh:mm:ss', function() {
+      expect(datetime(dt, 'yyyy-MM-dd hh:mm:ss').format()).to.be(dt)
+    })
+
+    it('week', function() {
+      expect(datetime(ts, 'EEEE').format()).to.be('Friday')
+      expect(datetime(ts, 'EEE').format()).to.be('Fri')
+      expect(datetime(ts, 'D').format()).to.be('5')
+    })
+
+    it('year', function() {
+      expect(datetime(ts, 'yyyy').format()).to.be('2015')
+      expect(datetime(ts, 'yy').format()).to.be('115')
+    })
+
+    it('month', function() {
+      expect(datetime(ts, 'MMMM').format()).to.be('April')
+      expect(datetime(ts, 'MMM').format()).to.be('Apr')
+      expect(datetime(ts, 'MM').format()).to.be('04')
+      expect(datetime(ts, 'M').format()).to.be('4')
+    })
+
+    it('date', function() {
+      expect(datetime(ts, 'dd').format()).to.be('10')
+      expect(datetime(ts, 'd').format()).to.be('10')
+    })
+
+    it('hours', function() {
+      expect(datetime(ts, 'hh').format()).to.be('14')
+      expect(datetime(ts, 'h').format()).to.be('14')
+    })
+
+    it('minutes', function() {
+      expect(datetime(ts, 'mm').format()).to.be('43')
+      expect(datetime(ts, 'm').format()).to.be('43')
+    })
+
+    it('seconds', function() {
+      expect(datetime(ts, 'ss').format()).to.be('05')
+      expect(datetime(ts, 's').format()).to.be('5')
+    })
+
+    it('milliseconds', function() {
+      expect(datetime(ts, 'ii').format()).to.be(ms)
+      expect(datetime(ts, 'i').format()).to.be(ms)
+    })
+
+  })
+
+  describe('datetime', function() {
+
+    it('yyyy-MM-dd hh:mm:ss', function() {
+      expect(datetime(dt, 'yyyy-MM-dd hh:mm:ss').format()).to.be(dt)
+    })
+
+    it('yyyy-MM-dd', function() {
+      expect(datetime(d, 'yyyy-MM-dd').format()).to.be(d)
+    })
+
+    it('hh:mm:ss', function() {
+      expect(datetime(t, 'hh:mm:ss').format()).to.be(t)
+    })
+
+    it('ii', function() {
+      expect(datetime(ms, 'ii').format()).to.be(ms)
+    })
+
+    it('toNumber()', function() {
+      expect(datetime(dtms, 'yyyy-MM-dd hh:mm:ss.ii').toNumber()).to.be(ts)
+    })
+
+  })
+
+  describe('dateobject', function() {
+
+    var dobj = new Date(ts);
+
+    it('yyyy-MM-dd hh:mm:ss', function() {
+      expect(datetime(dobj, 'yyyy-MM-dd hh:mm:ss').format()).to.be(dt)
+    })
+
+    it('yyyy-MM-dd', function() {
+      expect(datetime(dobj, 'yyyy-MM-dd').format()).to.be(d)
+    })
+
+    it('hh:mm:ss', function() {
+      expect(datetime(dobj, 'hh:mm:ss').format()).to.be(t)
+    })
+
+    it('ii', function() {
+      expect(datetime(dobj, 'ii').format()).to.be(ms)
+    })
+
+    it('toNumber()', function() {
+      expect(datetime(dobj, 'yyyy-MM-dd hh:mm:ss.ii').toNumber()).to.be(ts)
+    })
+
+    it('toString()', function() {
+      expect(datetime(dobj, 'yyyy-MM-dd hh:mm:ss.ii').toString()).to.be(dtms)
+    })
+
+  })
+
+})
+
+})
